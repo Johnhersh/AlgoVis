006f734a6339831014f6979222a527955c4d7744
John Hershberg <johnhershberg@Johns-MacBook-Pro-14814.local>
2020-06-07 22:59:58 -0700

    Added animation for Vertical Bar

--- src/components/verticalBar.component.tsx
+++ src/components/verticalBar.component.tsx
@@ -1,20 +1,23 @@
 // import React from "react";
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./verticalBar.styles.scss";
 
 export default function VerticalBar(props: any) {
-  let newHeight = (props.value / props.maxValue) * 400;
+  const [height, setHeight] = useState(0);
 
-  // Defining this here instead of the CSS because height uses a value from a prop
-  var barStyle = {
-    height: newHeight,
-  };
+  useEffect(() => {
+    window.setTimeout(updateHeight, 100);
+  });
+
+  function updateHeight() {
+    setHeight((props.value / props.maxValue) * 400);
+  }
 
   return (
     <div className="verticalBarContainer">
       <p className="verticalBarText">{props.value}</p>
-      <div className="verticalBarGradient" style={barStyle} />
+      <div className="verticalBarGradient" style={{ height: height }} />
     </div>
   );
 }
