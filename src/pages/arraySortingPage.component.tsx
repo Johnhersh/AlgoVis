5162e91091191027aef2fe2ac90be18f2d938150
John Hershberg <johnher@gmail.com>
2020-06-08 23:09:33 -0700

    Moved functions to their own file

--- src/pages/arraySortingPage.component.tsx
+++ src/pages/arraySortingPage.component.tsx
@@ -6,6 +6,7 @@ import ButtonPicker from "../components/buttonPicker.component";
 import { iterativeMergeSort, quickSort, bubbleSort } from "../algorithms/sortingAlgorithms";
 
 import "./arraySortingPage.styles.scss";
+import { getNumbersOnlyArray, newRandomArray } from "./arrayHelperFunctions";
 
 export default function ArraySortingPage() {
   const [desiredAmountOfValues, setNumberOfValues] = useState(15);
@@ -89,23 +90,4 @@ export default function ArraySortingPage() {
       </div>
     </div>
   );
-}
-
-function getNumbersOnlyArray(items: any) {
-  let newArray: Array<number> = [];
-
-  for (let index = 0; index < items.length; index++) {
-    newArray.push(items[index].value);
-  }
-
-  return newArray;
-}
-
-function newRandomArray(length: number) {
-  let newArray = [];
-  for (let index = 0; index < length; index++) {
-    newArray.push({ id: index, value: Math.floor(Math.random() * 100 + 1) });
-  }
-
-  return newArray;
 }
