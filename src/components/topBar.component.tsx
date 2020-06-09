95181afc445be54b546d4932b5b4baed59ccec82
John Hershberg <johnhershberg@Johns-MacBook-Pro-14814.local>
2020-06-08 20:47:38 -0700

    Added TopBar styling

--- src/components/topBar.component.tsx
+++ src/components/topBar.component.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, NavLink } from "react-router-dom";
 
 import "./topBar.styles.scss";
 
 export default function TopBar(props: any) {
-  return <div className="topBarContainer">{props.children}</div>;
+  return (
+    <div className="topBarContainer">
+      <nav>
+        <ul className="navLinksContainer">
+          <li className="linkItem">
+            <NavLink to="/sorting" className="navLink" activeClassName="navLinkActive">
+              Sorting
+            </NavLink>
+          </li>
+          <li className="linkItem">
+            <NavLink to="/fibonacci" className="navLink" activeClassName="navLinkActive">
+              Fibonacci
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 }
