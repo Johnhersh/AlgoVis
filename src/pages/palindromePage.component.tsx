ec8fa8d13937a929ab1fa0a890500ed6c3b44e58
John Hershberg <johnher@gmail.com>
2020-06-20 22:53:47 -0700

    Changed to functional component

--- src/pages/palindromePage.component.tsx
+++ src/pages/palindromePage.component.tsx
@@ -4,21 +4,44 @@ import "./palindromePage.styles.scss";
 import Palindrome from "../components/palindrome.component";
 import ButtonStringInput from "../components/buttonStringInput.component";
 
+import { isPalindrome } from "../algorithms/palindrome";
+
 export default function PalindromePage() {
+  const [palindromicCharacters, setPalindromicCharacters] = useState<Array<boolean>>([false]);
   const [currentCheckString, setCurrentCheckString] = useState("");
 
   function onWordUpdate(event: any) {
     if (event.target.value.length < 30) setCurrentCheckString(event.target.value);
   }
 
+  function checkPalindrome() {
+    let result = isPalindrome(currentCheckString);
+    if (result) {
+      const tempCharacterStateArray: Array<boolean> = [];
+      for (let index = 0; index < currentCheckString.length; index++) {
+        tempCharacterStateArray.push(true);
+        console.log("At length: " + currentCheckString.length);
+      }
+      setPalindromicCharacters(tempCharacterStateArray);
+    }
+    console.log("String is palindrome? : " + result);
+  }
+
   return (
     <div className="palindromePageContainer">
       <div className="palindromeResultContainer">
-        <Palindrome checkWord={currentCheckString} />
+        <Palindrome
+          checkWord={currentCheckString}
+          palindromicCharactersArray={palindromicCharacters}
+        />
       </div>
       <div className="buttonsContainer">
         <div style={{ flex: 1 }} />
-        <ButtonStringInput onChange={onWordUpdate} value={currentCheckString}>
+        <ButtonStringInput
+          onChange={onWordUpdate}
+          onLoseFocus={checkPalindrome}
+          value={currentCheckString}
+        >
           Word:
         </ButtonStringInput>
         <div style={{ flex: 1 }} />
