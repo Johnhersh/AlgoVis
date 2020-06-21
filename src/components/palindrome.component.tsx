a4a5ac6079b2dba41d5dcdd68f44ee0a9133d57e
John Hershberg <johnhershberg@Johns-MacBook-Pro-14814.local>
2020-06-20 22:53:47 -0700

    Changed to functional component

--- src/components/palindrome.component.tsx
+++ src/components/palindrome.component.tsx
@@ -2,6 +2,29 @@ import React, { useEffect, useState, useRef } from "react";
 
 import "./palindrome.styles.scss";
 
-export default function Palindrome(props: any) {
-  return <div className="palindromeContainer">{props.checkWord}</div>;
+interface Props {
+  palindromicCharactersArray: any;
+  checkWord: string;
 }
+
+const Palindrome: React.FC<Props> = ({ palindromicCharactersArray, checkWord }) => {
+  let textToRender = [];
+  console.log("length is: " + palindromicCharactersArray.length);
+  for (let index = 0; index < palindromicCharactersArray.length; index++) {
+    textToRender.push(<li style={{ display: "inline" }}>{checkWord.charAt(index)}</li>);
+  }
+
+  return <ul className="palindromeContainer">{textToRender}</ul>;
+};
+
+export default Palindrome;
+
+// export default function Palindrome({ palindromicCharactersArray, ...props }) {
+//   let textToRender = [];
+//   console.log("length is: " + palindromicCharactersArray.length);
+//   for (let index = 0; index < palindromicCharactersArray.length; index++) {
+//     textToRender.push(<li style={{ display: "inline" }}>A</li>);
+//   }
+
+//   return <ul className="palindromeContainer">{textToRender}</ul>;
+// }
