5cba7d40264edae7dd1475a71152e230dc1a770c
John Hershberg <johnher@gmail.com>
2020-06-24 23:09:25 -0700

    Added home to topbar. Added icons

--- src/components/topBar.component.tsx
+++ src/components/topBar.component.tsx
@@ -10,22 +10,31 @@ export default function TopBar(props: any) {
       <nav>
         <ul className="navLinksContainer">
           <li className="linkItem">
+            <NavLink to="/mainmenu" className="navLink" activeClassName="navLinkActive">
+              <i className="material-icons">home</i>
+            </NavLink>
+          </li>
+          <li className="linkItem">
             <NavLink to="/palindrome" className="navLink" activeClassName="navLinkActive">
+              <i className="material-icons">compare_arrows</i>
               Palindrome
             </NavLink>
           </li>
           <li className="linkItem">
             <NavLink to="/twosum" className="navLink" activeClassName="navLinkActive">
+              <i className="material-icons">add</i>
               Two-Number Sum
             </NavLink>
           </li>
           <li className="linkItem">
             <NavLink to="/fibonacci" className="navLink" activeClassName="navLinkActive">
+              <i className="material-icons">signal_cellular_alt</i>
               Fibonacci
             </NavLink>
           </li>
           <li className="linkItem">
             <NavLink to="/sorting" className="navLink" activeClassName="navLinkActive">
+              <i className="material-icons">format_list_numbered_rtl</i>
               Sorting
             </NavLink>
           </li>
